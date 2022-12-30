7b139461fe633d9cc6aed762bb3ff17e6e28ce70
German Eichemberger <germaneichemberger@gmail.com>
2022-12-30 10:00:40 -0300

    refactor command code

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,7 +1,11 @@
 use std::process::ExitCode;
-use std::env; 
+use std::env;
 use crate::service::account_service::{list_sites, find_by_site};
 use crate::utils::messages::print_account;
+
+const GET: &str ="get";
+const LIST: &str = "ls";
+const PASSWORD_OPTION : &str = "-p";
 
 pub fn execute_command() -> ExitCode  {
     let raw_args: Vec<String> = env::args().collect();
@@ -15,50 +19,8 @@ pub fn execute_command() -> ExitCode  {
     }
 
     match command.as_str() {
-        "ls" => list_sites(),
-        "get" => {
-            let site_optional = args.iter()
-                .skip(1)
-                .filter(|a| a.chars().next().unwrap() != '-')
-                .next();
-
-            if site_optional.is_none() {
-                println!("❗ The get command requires a site name");
-                return ExitCode::FAILURE;
-            }
-
-            let site = site_optional
-                .unwrap()
-                .to_lowercase();
-
-            let account_optional = find_by_site(&site);
-
-            if account_optional.is_none() {
-                println!("❗️ No account found for {}", site);
-                return ExitCode::from(102);
-            }
-
-            let account = account_optional.unwrap();
-
-            let options = args.iter()
-                .skip(1)
-                .filter(|a| a.chars().next().unwrap() == '-')
-                .next();
-
-            if options.is_none() {
-                print_account(&account);
-                return ExitCode::SUCCESS;
-            }
-
-            match options.unwrap().as_str() {
-                "-p" => println!("🔑 {}", account.password),
-                _ => {
-                    println!("Invalid option");
-                    return ExitCode::from(101);
-                },
-            }
-
-        },
+        LIST => list_sites(),
+        GET => return run_get(args),
         _ => {
             println!("Please enter a valid command");
             return ExitCode::FAILURE; 
@@ -66,4 +28,49 @@ pub fn execute_command() -> ExitCode  {
     }
 
     return ExitCode::SUCCESS;
+}
+
+fn run_get(args: Vec<&String>) -> ExitCode {
+    let site_optional = args.iter()
+    .skip(1)
+    .filter(|a| a.chars().next().unwrap() != '-')
+    .next();
+
+    if site_optional.is_none() {
+        println!("❗ The get command requires a site name");
+        return ExitCode::FAILURE;
+    }
+
+    let site = site_optional
+        .unwrap()
+        .to_lowercase();
+
+    let account_optional = find_by_site(&site);
+
+    if account_optional.is_none() {
+        println!("❗️ No account found for {}", site);
+        return ExitCode::FAILURE;
+    }
+
+    let account = account_optional.unwrap();
+
+    let options = args.iter()
+        .skip(1)
+        .filter(|a| a.chars().next().unwrap() == '-')
+        .next();
+
+    if options.is_none() {
+        print_account(&account);
+        return ExitCode::SUCCESS;
+    }
+
+    match options.unwrap().as_str() {
+        PASSWORD_OPTION => println!("🔑 {}", account.password),
+        _ => {
+            println!(" ❗ Invalid option");
+            return ExitCode::from(101);
+        },
+    }
+
+    ExitCode::SUCCESS
 }
--- src/dto/mod.rs
+++ src/dto/mod.rs
@@ -1,2 +1,2 @@
 pub mod account; 
-pub mod menu_option; 
+pub mod menu_option;
